254182c9768c52063fe33e7f64abe9d621392f8c
Bret Richdale <bret@instance.co.nz>
2023-02-16 21:10:14 +1300

    Add topicsSlice

---
+++ src/features/topics/TopicsSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const topicsSlice = createSlice({
+  name: "topics",
+  initialState: {
+    topics: {}
+  },
+  reducers: {
+    addTopic: (state, action) => {
+      // format: {id: '123456', name: 'name of topic', icon: 'icon url'}
+      const newTopic = action.payload;
+      state.topics[action.payload.id] = {
+        id: newTopic.id,
+        name: newTopic.name,
+        icon: newTopic.icon,
+        quizIds: []
+      }
+    }
+  }
+}
+);
+
+export const selectTopics = (state) => state.topics;
+export default topicsSlice.reducers;
